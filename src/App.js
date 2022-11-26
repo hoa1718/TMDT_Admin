602a31c1b75eafab8b882dcaf117166dfe41950f
hoa1718 <82604780+hoa1718@users.noreply.github.com>
2022-11-26 11:07:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Admin from "./Admin";
 import Login from "./Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  const basename = document.querySelector('base')?.getAttribute('href') ?? '/'
   return (
     <div className="App" id="wrapper">
-      <BrowserRouter>
+      <BrowserRouter basename={basename} />
         <Routes>
             <Route path="/M/*" element={<Admin></Admin>}></Route>
             <Route path="/Login" element={<Login></Login>}></Route>
