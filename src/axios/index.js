256f43b83898cb23fc678abfce18b80e8f21050a
mtpodana <101807165+mtpodana@users.noreply.github.com>
2022-11-28 18:39:08 +0700

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosClient = axios.create({
     baseURL: 'http://localhost:4000/',
-    timeout: 1000,
     headers: {'Content-Type': 'application/json'}
   });
 
