be8b6825a2720e060ae0d213c3f1adb9fe477952
mtpodana <101807165+mtpodana@users.noreply.github.com>
2022-11-30 21:57:42 +0700

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://tmdt-server.herokuapp.com/',
     timeout: 5000,
     headers: {'Content-Type': 'application/json'}
   });
